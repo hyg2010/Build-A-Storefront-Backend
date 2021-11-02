9833237c4b629e0d101aa92851ed641786932104
hyg2010 <81759800+hyg2010@users.noreply.github.com>
2021-11-01 18:55:02 -0700

    update

--- src/database.ts
+++ src/database.ts
@@ -13,10 +13,27 @@ const {
 } = process.env;
 
 
-const client = new Pool({
+let client: Pool = new Pool();
+console.log(ENV)
+
+
+if(ENV === 'test') {
+  client = new Pool({
   host: POSTGRES_HOST,
-  database: ENV === "dev" ? POSTGRES_DB : POSTGRES_TEST_DB,
+  database: POSTGRES_TEST_DB,
   user: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
-});
+  })
+}
+
+if (ENV === 'dev') {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  })
+}
+
+
 export default client; 
